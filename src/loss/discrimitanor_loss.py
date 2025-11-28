5a65c28b420e893ec573da04d3c8978e179fcc48
7embl4 <art3mka1408@gmail.com>
2025-11-28 12:11:13 +0300

    Add generator and discriminator losses

---
+++ src/loss/discrimitanor_loss.py
@@ -0,0 +1,20 @@
+from typing import List
+
+import torch
+from torch import nn
+
+
+class DiscriminatorLoss(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, mpd_rs, mpd_gs, msd_rs, msd_gs, **batch):
+        mpd_loss = 0.0
+        for i in range(len(mpd_rs)):
+            mpd_loss = mpd_loss + torch.mean((mpd_rs[i] - 1) ** 2 + mpd_gs[i] ** 2)
+
+        msd_loss = 0.0
+        for i in range(len(msd_rs)):
+            msd_loss = msd_loss + torch.mean((msd_rs[i] - 1) ** 2 + msd_gs[i] ** 2)
+
+        return mpd_loss + msd_loss
