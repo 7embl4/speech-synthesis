207598ee4fa72857f422635a81690b1fb52bdf9c
7embl4 <art3mka1408@gmail.com>
2025-12-03 16:09:06 +0300

    Add neural MOS using NISQA lib

--- src/metrics/neural_mos.py
+++ src/metrics/neural_mos.py
@@ -1,40 +1,80 @@
 import os
+import shutil
 
 import torch
 import torchaudio
-import utmosv2
-from torch import nn
 
+from src.metrics.base_metric import BaseMetric
+from src.metrics.nisqa.NISQA_lib import predict_mos
+from src.metrics.nisqa.NISQA_model import nisqaModel
 from src.utils.io_utils import ROOT_PATH
 
 
-class NeuralMOS(nn.Module):
+class NeuralMOS(BaseMetric):
     """
-    Calcs MOS using UTMOSv2 model
+    Calcs MOS using NISQA model
     """
 
-    def __init__(self):
-        super().__init__()
+    def __init__(
+        self, model_path, mode="predict_dir", target_sr=22050, *agrs, **kwargs
+    ):
+        super().__init__(*agrs, **kwargs)
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
-        self.model = utmosv2.create_model(pretrained=True, device=self.device)
-        self.temp_dir = ROOT_PATH / "data/temp"
+        self.temp_dir = ROOT_PATH / "data" / "temp"
+
+        self.model_path = model_path
+        self.target_sr = target_sr
+        self.mode = mode
 
     @torch.no_grad()
-    def forward(self, gen_audio: torch.Tensor, **batch):
+    def __call__(self, gen_audio: torch.Tensor, **batch):
         """
         Saves generated audio and calculates metric
         """
         B, C, T = gen_audio.shape
+
+        # save generated audio
         os.makedirs(str(self.temp_dir), exist_ok=True)
         for i in range(B):
-            torchaudio.save(f"data/temp/gen_audio_{i}.wav", gen_audio[i], 22050)
+            torchaudio.save(
+                str(self.temp_dir / f"gen_audio_{i}.wav"), gen_audio[i], self.target_sr
+            )
 
-        out = self.model.predict(input_dir=str(self.temp_dir), verbose=False)
-        res = 0
-        for r in out:
-            print(r["predicted_mos"])
-            res += r["predicted_mos"]
+        # load model
+        nisqa_model, args = self._load_model(self.model_path)
 
-        os.remove(self.temp_dir)
-        res = torch.tensor(res)
-        return res.mean()
+        # make predictions
+        predict_mos(
+            nisqa_model.model,
+            nisqa_model.ds_val,
+            args["tr_bs_val"],
+            self.device,
+            num_workers=args["tr_num_workers"],
+        )
+        res_df = nisqa_model.ds_val.df
+        total_mos = res_df["mos_pred"].mean()
+
+        shutil.rmtree(self.temp_dir)
+        return total_mos
+
+    def _load_model(self, model_path):
+        checkpoint = torch.load(model_path, map_location=self.device)
+
+        args = {}
+        args.update(checkpoint["args"])
+        args.update(
+            {
+                "mode": self.mode,
+                "pretrained_model": model_path,
+                "data_dir": self.temp_dir,
+                "output_dir": None,
+                "tr_bs_val": 1,
+                "tr_num_workers": 0,
+                "tr_device": self.device,
+                "ms_channel": None,
+                "ms_sr": self.target_sr,
+                "ms_fmax": self.target_sr // 2,
+            }
+        )
+
+        return nisqaModel(args), args
